2d537e2af5d7c55b289601afb458f928bc8c009b
Rio Richard <riorichard12@gmail.com>
2024-04-20 21:33:21 +0700

    loading env done

---
+++ pkg/config/envLoad.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func LoadEnv() {
+	err := loadEnvFromFile(".env")
+	if err != nil {
+		panic(fmt.Sprintf("cannot load env variables with erro: %v", err.Error()))
+	}
+}
+
+func loadEnvFromFile(filename string) error {
+	// Open the .env file
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// Read the file line by line
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		// Split each line into key-value pairs
+		parts := strings.SplitN(line, "=", 2)
+		if len(parts) != 2 {
+			continue // Skip invalid lines
+		}
+		// Set the environment variable
+		os.Setenv(parts[0], parts[1])
+	}
+
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+
+	return nil
+}
