4d2ec760215257b11526a45aa0f8557fc94a38c0
Rio Richard <riorichard12@gmail.com>
2024-04-21 14:42:17 +0700

    config path with no jwt needed

--- internal/interface/server/http/middleware/jwtMiddleware.go
+++ internal/interface/server/http/middleware/jwtMiddleware.go
@@ -10,17 +10,27 @@ import (
 )
 
 func JWTMiddleware(secretKey string) echo.MiddlewareFunc {
-	return middleware.JWTWithConfig(middleware.JWTConfig{
-		Claims:        &jwt.StandardClaims{},
-		SigningKey:    []byte(secretKey),
-		SigningMethod: "HS256",
-		ErrorHandler: func(err error) error {
-			if strings.Contains(err.Error(), "Token is expired") {
-				return echo.NewHTTPError(http.StatusUnauthorized, "Token is expired")
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			// Check if the request path starts with "/onboard" or is "/ping"
+			if strings.HasPrefix(c.Request().URL.Path, "/onboard") || c.Request().URL.Path == "/ping" {
+				// Allow access to endpoints starting with "/onboard" and the "/ping" endpoint without requiring a token
+				return next(c)
 			}
 
-			// Handle other JWT validation errors
-			return echo.NewHTTPError(http.StatusUnauthorized, "Invalid or missing token")
-		},
-	})
+			// For other endpoints, perform JWT authentication
+			return middleware.JWTWithConfig(middleware.JWTConfig{
+				Claims:        &jwt.StandardClaims{},
+				SigningKey:    []byte(secretKey),
+				SigningMethod: "HS256",
+				ErrorHandler: func(err error) error {
+					if strings.Contains(err.Error(), "Token is expired") {
+						return echo.NewHTTPError(http.StatusUnauthorized, "Token is expired")
+					}
+					// Handle other JWT validation errors
+					return echo.NewHTTPError(http.StatusUnauthorized, "Invalid or missing token")
+				},
+			})(next)(c)
+		}
+	}
 }
