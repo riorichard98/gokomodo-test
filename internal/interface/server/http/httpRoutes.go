8b165d2814806dc66be91deabd41d41e5877b761
Rio Richard <riorichard12@gmail.com>
2024-04-21 23:01:53 +0700

    remove jwt middleware and adding login and add new product

--- internal/interface/server/http/httpRoutes.go
+++ internal/interface/server/http/httpRoutes.go
@@ -4,25 +4,24 @@ import (
 	"net/http"
 
 	"gokomodo-test/internal/interface/container"
-	"gokomodo-test/pkg/config"
-	// "gokomodo-test/internal/interface/server/http/httpHandler"
-
-	"gokomodo-test/internal/interface/server/http/middleware"
+	"gokomodo-test/internal/interface/server/http/httpHandler"
 
 	"github.com/labstack/echo/v4"
 )
 
 func SetupRouter(server *echo.Echo, cont *container.Container) {
-	// handler := httpHandler.SetupHandlers(cont)
+	handler := httpHandler.SetupHandlers(cont)
 
 	// - health check
 	server.GET("/ping", func(ctx echo.Context) error {
 		return ctx.String(http.StatusOK, "services up and running... ")
 	})
 
-	// onboardRoute := server.Group("/onboard")
+	onboardRoute := server.Group("/onboard")
+	onboardRoute.POST("/login", handler.OnboardHandler.LoginHandler)
 
-	server.Use(middleware.JWTMiddleware(config.GetEnvString("JWT_SECRET_KEY")))
+	sellerRoute := server.Group("/sellers")
+	sellerRoute.POST("/product", handler.SellerHandler.AddNewProduct)
 
 	// buyerRoute := server.Group("/buyer")
 	// sellerRoute := server.Group("/seller")
